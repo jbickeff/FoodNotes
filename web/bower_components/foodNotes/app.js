c7efc6f6649e9c61d3fca2378efb6c762f2ab479
Gage Peterson <justgage@gmail.com>
2014-07-08 21:23:33 -0600

    Added pagation and bootstrap style

--- web/bower_components/foodNotes/app.js
+++ web/bower_components/foodNotes/app.js
@@ -1,8 +1,12 @@
 (function() {
+   "use strict";
    var app = angular.module('foodNotes', []);
 
 
 
+   /**
+    * A Controller for handling user information
+    */
    app.controller('UserController', function($scope, $http) {
       var promise = $http.get("/bower_components/foodNotes/name.json");
 
@@ -20,12 +24,15 @@
 
 
 
+   /**
+    * A controller that controlls the history tab
+    */
    app.controller('HistoryController', function($scope, $http) {
       $scope.days = [];
+      $scope.hist = [];
+      $scope.offset = 0;
 
       var promise = $http.get("/bower_components/foodNotes/hist.json");
-
-      $scope.hist = [];
 
       promise.success(function (data, status, headers, config) {
          $scope.hist = data;
@@ -34,6 +41,32 @@
       promise.error(function (data, status, headers, config) {
          console.error(data, status, headers, config);
       });
+
+
+      this.newest = function() {
+         $scope.offset = 0;
+      };
+
+
+      this.next = function() {
+         if($scope.offset < $scope.hist.length - 3) {
+            $scope.offset += 3;
+         }
+      };
+
+      this.prev = function() {
+         $scope.offset -= 3;
+
+         if ($scope.offset < 0) {
+            $scope.offset = 0;
+         }
+      };
+
+      this.getHist = function() {
+         var max = $scope.hist.length - $scope.offset - 3 >= 3 ? 3 : $scope.hist.length - $scope.offset;
+         return $scope.hist.slice($scope.offset, $scope.offset + max);
+      };
+
 
    });
 
